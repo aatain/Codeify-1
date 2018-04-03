3629830488daa7170599ee8d4b2a132458e2fb13
Alyssa Atain <alyssaatain@Alyssas-MacBook-Pro.local>
2018-04-03 10:28:56 -0700

    Merging Day 1 changes with master

--- server/controllers/submissionController.js
+++ server/controllers/submissionController.js
@@ -3,7 +3,7 @@ const db = require('../models/modelForAlg.js');
 const submissionController = {};
 
 submissionController.submitCode = (req, res) => {
-    db.query(`INSERT INTO submissions (code, username, submitted_at, alg_id) VALUES ('${req.body.code}', '${req.body.username}', '${req.body.submitted_at}', '${req.body.alg_id}');`,
+    db.query(`INSERT INTO submissions (code, username, alg_id) VALUES ('${req.body.code}', '${req.body.username}', '${req.body.submitted_at}', '${req.body.alg_id}');`,
         (err, result) => {
             if (err) throw new Error(err);
             res.send('successfully posted');
@@ -12,5 +12,9 @@ submissionController.submitCode = (req, res) => {
 };
 
 //add method for displaying submissions
+submissionController.populateGallery = (req, res) => {
+    db.query(
+    );
+};
 
 module.exports = submissionController;
